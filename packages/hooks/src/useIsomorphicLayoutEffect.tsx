e7ee2b1e66e0949e934edcf73ca7a351a30d7aa7
OdysseyCreations <odysseyworldart@gmail.com>
2021-03-01 17:36:21 -0800

    button dir

--- packages/hooks/src/useIsomorphicLayoutEffect.tsx
+++ packages/hooks/src/useIsomorphicLayoutEffect.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { canUseDOM } from '@apt-ui/utils';
 
-// export const useIsomorphicLayoutEffect = canUseDOM()
-//   ? React.useLayoutEffect
-//   : React.useEffect;
+export const useIsomorphicLayoutEffect = canUseDOM()
+  ? React.useLayoutEffect
+  : React.useEffect;
